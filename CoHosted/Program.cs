da2a0d1f297a0b96d4657ccdc41b5b22480543d2
Ruslan Tatarinov <volinc@users.noreply.github.com>
2022-12-24 02:40:34 +0200

    ...

--- CoHosted/Program.cs
+++ CoHosted/Program.cs
@@ -3,61 +3,68 @@ using Orleans.Serialization;
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-var host = Host.CreateDefaultBuilder(args)
-    .UseOrleans((context, siloBuilder) =>
-    {
-        var clusterId = context.Configuration.GetValue<string>(Constants.ClusterIdKey);
-        var serviceId = context.Configuration.GetValue<string>(Constants.ServiceIdKey);
-        var connectionString = context.Configuration.GetConnectionString(Constants.ConnectionStringKey);
-        var isKubernetesHosting = context.Configuration.GetValue<bool>(Constants.IsKubernetesHostingKey);
+try
+{
+    var host = Host.CreateDefaultBuilder(args)
+        .UseOrleans((context, siloBuilder) =>
+        {
+            var clusterId = context.Configuration.GetValue<string>(Constants.ClusterIdKey);
+            var serviceId = context.Configuration.GetValue<string>(Constants.ServiceIdKey);
+            var connectionString = context.Configuration.GetConnectionString(Constants.ConnectionStringKey);
+            var isKubernetesHosting = context.Configuration.GetValue<bool>(Constants.IsKubernetesHostingKey);
 
-        siloBuilder.Configure<ClusterOptions>(options =>
-        {
-            options.ClusterId = clusterId;
-            options.ServiceId = serviceId;
-        })
-        .UseAdoNetClustering(options =>
-        {
-            options.Invariant = Constants.Invariant;
-            options.ConnectionString = connectionString;
-        })
-        .AddAdoNetGrainStorageAsDefault(options =>
-        {
-            options.Invariant = Constants.Invariant;
-            options.ConnectionString = connectionString;
-        })
-        .UseAdoNetReminderService(options =>
-        {
-            options.Invariant = Constants.Invariant;
-            options.ConnectionString = connectionString;
-        })
-        .ConfigureEndpoints(siloPort: 11112, gatewayPort: 30001)
-        .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Debug).AddConsole());
-
-        if (isKubernetesHosting)
-            siloBuilder.UseKubernetesHosting();
-    })
-    .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.ConfigureServices(services =>
-        {
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-        });
-        webBuilder.Configure((ctx, app) =>
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
+            siloBuilder.Configure<ClusterOptions>(options =>
             {
-                endpoints.MapControllers();
-            });
-        });
-    })
-    .Build();
+                options.ClusterId = clusterId;
+                options.ServiceId = serviceId;
+            })
+            .UseAdoNetClustering(options =>
+            {
+                options.Invariant = Constants.Invariant;
+                options.ConnectionString = connectionString;
+            })
+            .AddAdoNetGrainStorageAsDefault(options =>
+            {
+                options.Invariant = Constants.Invariant;
+                options.ConnectionString = connectionString;
+            })
+            .UseAdoNetReminderService(options =>
+            {
+                options.Invariant = Constants.Invariant;
+                options.ConnectionString = connectionString;
+            })
+            .ConfigureEndpoints(siloPort: 21111, gatewayPort: 40000)
+            .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Debug).AddConsole());
 
-await host.RunAsync();
+            if (isKubernetesHosting)
+                siloBuilder.UseKubernetesHosting();
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.ConfigureServices(services =>
+            {
+                services.AddControllers();
+                services.AddEndpointsApiExplorer();
+                services.AddSwaggerGen();
+            });
+            webBuilder.Configure((ctx, app) =>
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            });
+        })
+        .Build();
+
+    await host.RunAsync();
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception);
+    throw;
+}
